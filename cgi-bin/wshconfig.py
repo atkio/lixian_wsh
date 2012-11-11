4181b62e9ffb521ccc90b343a6eee20c912b77d1
atkio <atkio@ymail.com>
2012-11-11 18:48:31 +0900

    bug name wrong

--- cgi-bin/wshconfig.py
+++ cgi-bin/wshconfig.py
@@ -4,25 +4,25 @@ import sys,os,os.path,json
 WSH_CONFIG = "/tmp/lixian/.lixian_wsh.conf"
 
 def readconf():
-	try:
-		config= json.loads(file(WSH_CONFIG).read())
-		os.environ['LIXIAN_HOME'] = conf['LIXIAN_HOME']
-		return config
-	except:
+	if not os.path.exists(WSH_CONFIG):
 		str='{"LIXIAN_HOME":"/tmp/lixian","LIXIAN_DOWNLOAD_PATH":"/tmp/lixian/download","username":"username","password":"password","pretask":"","aftertask":""}'
 		os.environ['LIXIAN_HOME'] = "/tmp/lixian"
-		file(WSH_CONFIG, "w").write(str)  
-		return json.loads(str)
+		file(WSH_CONFIG, "w+").write(str)
+		
+	config= json.loads(file(WSH_CONFIG,"r").read())
+	os.environ['LIXIAN_HOME'] = config['LIXIAN_HOME']
+	return config
 		
 
 def readconfstr():
-	try:
-		return file(WSH_CONFIG).read()
-	except:
+	if not os.path.exists(WSH_CONFIG):
 		str='{"LIXIAN_HOME":"/tmp/lixian","LIXIAN_DOWNLOAD_PATH":"/tmp/lixian/download","username":"username","password":"password","pretask":"","aftertask":""}'
 		os.environ['LIXIAN_HOME'] = "/tmp/lixian"
-		file(WSH_CONFIG, "w").write(str)  
-		return str
+		file(WSH_CONFIG, "w+").write(str)
+	
+	return file(WSH_CONFIG,"r").read()
+		
 		
 def writeconf(data):
-	file(WSH_CONFIG, "w").write(json.dumps(data)) 
+	file(WSH_CONFIG, "w+").write(json.dumps(data)) 
+	
